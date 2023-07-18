689de8167791249d1b48b276181d3a21e1bb28fe
Saloni <salonipanchamia29@gmail.com>
2023-07-18 10:47:37 -0400

    Initial Commit

---
+++ ToDolist/ToDolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDolistApp.swift
+//  ToDolist
+//
+//  Created by Saloni Panchamia on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
